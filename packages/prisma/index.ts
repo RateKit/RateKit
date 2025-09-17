884316e7a2faefcd2350c271d0e37353e4b94cf4
Yaniv <yanivdouieb@icloud.com>
2025-09-17 17:14:54 +0200

    feat(prisma): initialize Prisma client with dynamic logging

---
+++ packages/prisma/index.ts
@@ -0,0 +1,31 @@
+import { type Prisma, PrismaClient } from "./client";
+
+const prismaOptions: Prisma.PrismaClientOptions = {};
+
+const globalForPrisma = global as unknown as {
+	baseClient: PrismaClient;
+};
+
+const loggerLevel = parseInt(process.env.NEXT_PUBLIC_LOGGER_LEVEL ?? "", 10);
+
+if (!Number.isNaN(loggerLevel)) {
+	switch (loggerLevel) {
+		case 1:
+			prismaOptions.log = ["error"];
+			break;
+		case 2:
+			prismaOptions.log = ["warn", "error"];
+			break;
+		case 3:
+			prismaOptions.log = ["info", "warn", "error"];
+			break;
+		case 4:
+			prismaOptions.log = ["query", "info", "warn", "error"];
+			break;
+		default:
+			break;
+	}
+}
+
+export const prisma =
+	globalForPrisma.baseClient || new PrismaClient(prismaOptions);
