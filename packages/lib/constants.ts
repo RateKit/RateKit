a9ea3ef017c53595b3b5a7daacd0eb2bcf506ad1
Yaniv <yanivdouieb@icloud.com>
2025-09-17 22:29:12 +0200

    feat(constants): add API_TAGS for authentication

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -1,2 +1,5 @@
 export const APP_NAME = process.env.APP_NAME ?? "RateKit";
 export const LOGGER_LEVEL = parseInt(process.env.LOGGER_LEVEL ?? "3", 10);
+export const API_TAGS = {
+	auth: "Authentication",
+};
