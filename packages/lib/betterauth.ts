5f9959939eb3307e0ade15df21cf08fa18657a8c
Yaniv <yanivdouieb@icloud.com>
2025-09-17 17:44:25 +0200

    feat(auth): implement better-auth integration with Prisma

---
+++ packages/lib/betterauth.ts
@@ -0,0 +1,11 @@
+import prisma from "@ratekit/prisma";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { APP_NAME } from "./constants";
+
+export const auth = betterAuth({
+	appName: APP_NAME,
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+});
